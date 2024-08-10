71436124a7d433529579dbd61da0373de9ef17ce
Muslim khan <141915047+muslim11222@users.noreply.github.com>
2024-08-09 18:41:30 -1200

    Add files via upload
    okay

---
+++ full-website/Fat Arrow Function.js
@@ -0,0 +1,25 @@
+
+
+// var number = () => {
+//      return 10;
+// }
+
+// console.log(number());
+
+
+// var number = () =>10;
+// console.log(number());;
+
+
+
+
+// 2. Truthy / Falsy values
+
+
+var myvar = true;
+
+if(myvar) {
+     console.log("i am truthy");
+} else {
+     console.log("i am not falsey");
+}
